68bc6fd58ae1800f24a7c3cbab6139b42a98d704
reema-code <karimaelgannoti@gmail.com>
2026-09-05 15:44:13 +0400

    Add Al Hamdani logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+export default function Logo() {
+  return (
+    <div className="logo-wrap" aria-label="الحمداني للحلويات المغربية">
+      <span className="logo-mark">✦</span>
+      <div>
+        <strong>الحمداني</strong>
+        <small>للحلويات المغربية</small>
+      </div>
+    </div>
+  );
+}
